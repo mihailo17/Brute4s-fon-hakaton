b48592b400bca30902d3d959b094b4d3e56a4274
djole94 <djordje.pantelic@datacomplete.net>
2020-12-05 10:49:53 +0100

    receiver register/login logic added

--- server/routes/receiverRouter.js
+++ server/routes/receiverRouter.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = new express.Router();
 const addReceiver = require('../receiverLogic/addReceiver.js');
+const receiverLogin = require('../receiverLogic/receiverLogin.js');
 
 router.post('/receivers', (req, res) => {
   addReceiver(req, res);
 });
 
+router.post('/receivers/login', (req, res) => {
+  receiverLogin(req, res);
+});
 
 module.exports = router;
