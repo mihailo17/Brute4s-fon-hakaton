e3c45b55bb6a837d0203888b4fca55071f8144b7
mihailo17 <mihailomilojkovic@yahoo.com>
2020-12-05 20:39:49 +0100

    Update pages content and build different forms

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,26 @@
 import React from 'react'
-import Directions from './Directions'
-import { GoogleMap, LoadScript, Marker, DirectionsRenderer, DirectionsService } from '@react-google-maps/api';
-
-const containerStyle = {
-  width: '100vw',
-  height: '100vh'
-};
- 
-const center = {
-  lat: 44.77328254755136, 
-  lng: 20.475249457670806
-};
-
+import './App.css';
+//import Directions from './components/Directions'
+import Giver from './components/Giver'
+import Header from './components/Header'
+import Homepage from './components/Homepage'
+import Receiver from './components/Receiver'
+import Register from './components/Register'
+import {Route, BrowserRouter as Router} from "react-router-dom"
 
 function App() {
-  const [markers, setMarkers] = React.useState([])
-  const [map, setMap] = React.useState(null)
-
-  const onMapClick = React.useCallback((event) => {
-    setMarkers((current) => [
-      ...current,
-      {
-       lat: event.latLng.lat(),
-       lng: event.latLng.lng(),
-       time: new Date()
-    }]);
-  }, []);
-
+  
   return (
-    <LoadScript
-      googleMapsApiKey="AIzaSyAMBqfNf9HiDaVKKMilXtupWqc4sWebse4"
-    >
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={5}
-        onClick={onMapClick}
-      >
-        { /* Child components, such as markers, info windows, etc. */ 
-          markers.map((marker) => (
-            <Marker
-               key={marker.time.toISOString()}
-               position={{lat: marker.lat, lng: marker.lng}}
-            >
-            </Marker>
-         ))
-        }
-        <Directions/>
-      </GoogleMap>
-    </LoadScript>
+    <>
+    <Header/>
+    <Router>
+      <Route path="/" exact component={Homepage}/>
+      <Route path="/giver" component={Giver}/>
+      <Route path="/receiver" component={Receiver}/>
+      <Route path="/register" component={Register}/>
+      {/* <Route path="/" component={Directions}/> */}
+    </Router>
+    </>
   )
 }
  
