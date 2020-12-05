68d0766b145c70bf656a654b3230e65691625f4f
djole94 <djordje.pantelic@datacomplete.net>
2020-12-05 08:58:38 +0100

    giver register implemented

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://cba:cba@cluster0.ecpxh.mongodb.net/hack?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://cba:cba@cluster0.ecpxh.mongodb.net/fonhakaton?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
--- server/routes/giverRouter.js
+++ server/routes/giverRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = new express.Router();
 const addGiver = require('../giverLogic/addGiver.js');
 
-router.post('/addGiver', (req, res) => {
+router.post('/givers', (req, res) => {
   addGiver(req, res);
 });
 
