4dff16e2345e9aab03d3e5329bc167cf1938889e
mihailo17 <mihailomilojkovic@yahoo.com>
2020-12-05 16:48:19 +0100

    Create custom components for different pages and functionalities

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <header>
+      <div className="logo">
+        <img alt="logo" src="../../public/logo.svg"></img>
+      </div>
+      <div className="user-profile"></div>
+    </header>
+  )
+}
---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Homepage() {
+  return <div> Initial page shown when you come to the appplication</div>
+}
