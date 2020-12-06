0dfe865110b2caa9e15de06aab1096712afd2be6
CaneLP <canelp94@gmail.com>
2020-12-06 01:29:52 +0100

    create optimal pathfinder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Homepage from './components/Homepage'
 import Receiver from './components/Receiver'
 import Register from './components/Register'
 import {Route, BrowserRouter as Router} from "react-router-dom"
+import MapWithMarkers from './components/MapWithMarkers';
 
 function App() {
   
@@ -19,7 +20,7 @@ function App() {
       <Route path="/giver" component={Giver}/>
       <Route path="/receiver" component={Receiver}/>
       <Route path="/register" component={Register}/>
-      {/* <Route path="/" component={Directions}/> */}
+      <Route path="/pathfinding" component={MapWithMarkers}/>
     </Router>
     </>
   )
