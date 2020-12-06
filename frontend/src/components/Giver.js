755fdfb9721f5b4fb2777d408d08eab84f2f8a9d
mihailo17 <mihailomilojkovic@yahoo.com>
2020-12-06 11:42:38 +0100

    Add some giver form functionality

--- frontend/src/components/Giver.js
+++ frontend/src/components/Giver.js
@@ -2,15 +2,18 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import GarbageItem from './GarbageItem';
 
+
 export default class Giver extends Component{
   constructor(props) {
     super(props);
     this.state = {
-      usersGarbage: []
+      usersGarbage: [],
     }
   }
-  async componentDidMount() {
+  
     
+  async componentDidMount() {
+      
     const token = sessionStorage.getItem("user-token");
     
     const response = await axios.get("http://localhost:8090/givers/getMyProducts", { 
@@ -22,15 +25,17 @@ export default class Giver extends Component{
     this.setState({
       usersGarbage: garbage,
     })
-    console.log(this.state.usersGarbage);
-    this.forceUpdate();    
+    this.forceUpdate();   
+    
   }
 
   async submitGiverForm(event) {
-    const radio = document.querySelectorAll('[name="radio"]');
+    const materialQuality = document.getElementById("materialQuality").value;
+    const materialQuantity = document.getElementById("quantity").value
     event.preventDefault();
-    const response = await axios.post("http://localhost:8090/givers",);
-
+    
+    const response = await axios.post("http://localhost:8090/givers", );
+    
   }
   
   render(){
@@ -58,39 +63,20 @@ export default class Giver extends Component{
 
         <h2>Prijavi otpad za sakupljanje:</h2>
         <form onSubmit={this.submitGiverForm} id="giver-submit-form">
-          
-          <div className="form-group mb-3">
-            <label htmlFor="adresa">Adresa predaje: </label>
-            <br></br>
-            <input className="form-control-lg" type="text" name="adresa" id="adresa"></input>
-          </div>     
   
-          <div className="form-group"></div>
-  
-          <div className="mb-1">
-            <input className="" type="radio" id="metal" name="radio" value="metal"></input>
-            <label className="" htmlFor="metal">Metal</label>
-          </div>
-          
-          <div className="mb-1">
-            <input className="" type="radio" id="staklo" name="radio" value="staklo"></input>
-            <label className="" htmlFor="staklo">Staklo</label>
-          </div>
-          
-          <div className="custom-control custom-checkbox mb-1">
-            <input className="" type="radio" id="papir" name="radio" value="papir"></input>
-            <label className="" htmlFor="papir">Papir</label>
-          </div>
-          
-          <div className="mb-1">
-            <input className="" type="radio" id="plastika" name="radio" value="plastika"></input>
-            <label className="" htmlFor="plastika">Plastika</label>
+          <div className="form-group">
+          <select name="material-quality" id="material-quality">
+            <option value="metal">Metal</option>
+            <option value="plastika">Plastika</option>
+            <option value="staklo">Staklo</option>
+            <option value="papir">Papir</option>
+          </select>
           </div>
           
           <div className="form-group mb-3">
-            <label htmlFor="kolicina">Kolicina u kilogramima</label>
+            <label htmlFor="quantity">Kolicina u kilogramima</label>
             <br></br>
-            <input className="form-control-lg" type="number" name="kolicina" id="kolicina"></input>
+            <input className="form-control" type="number" name="quantity" id="quantity"></input>
           </div>
               
           <input className="btn btn-primary" type="submit" value="Posalji"></input>
@@ -98,5 +84,4 @@ export default class Giver extends Component{
       </div>
       )
      }
-  
   }
