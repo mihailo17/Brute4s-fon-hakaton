fbc6fe65f7bf1c858f228488b286f647d6d88214
mihailo17 <mihailomilojkovic@yahoo.com>
2020-12-06 14:58:04 +0100

    Add frontend logic for giver page

--- frontend/src/components/MapGiver.js
+++ frontend/src/components/MapGiver.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 import { GoogleMap, LoadScript, Marker} from '@react-google-maps/api';
 
 
 const containerStyle = {
-  width: '100vw',
-  height: '100vh'
+  width: '100%',
+  height: '300px'
 };
 
 // Map starting center point
@@ -23,14 +24,34 @@ class MapGiver extends Component {
   
   constructor (props) {
     super(props);
-
     this.state = {
       marker: {
             position: center
         },
+      productQuantity: this.props.productQuantity,
+      productType: this.props.productType,
+      token: this.props.token
     };
-    
     this.onMapClick = this.onMapClick.bind(this)
+    this.submitForm = this.submitForm.bind(this)
+  }
+  async submitForm() {
+    
+    // const response = await axios.post("http://localhost:8090/givers/addProduct", 
+    // {
+    // "productType": this.state.productType,
+    // "productQuantity": this.state.productQuantity,
+    // "lat": this.state.marker.position.lat,
+    // "lng": this.state.marker.position.lng,
+    // "stateOfProduct": "open"
+    // },
+    // {
+    //   headers: {
+    //     "Authorization": `Bearer ${this.state.token}`
+    //   }
+    // },
+    // );
+    // console.log(response)
   }
 
   onMapClick = event => {
@@ -43,14 +64,17 @@ class MapGiver extends Component {
           } 
       }
     });
-
-    // console.log(this.state.markers);
+    
+    //console.log(this.state.marker);
     // console.log(this.context);
   };
 
   render () {
       if(this.state.marker){
         return (
+          <>
+          <button onClick={this.submitForm} className="btn btn-primary" type="submit" value="Posalji">Posalji</button>
+          <div className="map">
             <LoadScript
               googleMapsApiKey="AIzaSyAMBqfNf9HiDaVKKMilXtupWqc4sWebse4"
             >
@@ -68,6 +92,8 @@ class MapGiver extends Component {
                 }
               </GoogleMap>
             </LoadScript>
+            </div>
+            </>
           )
       } else {
         return (
